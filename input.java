d64fd21bf9c51d659221ba87067e81bc468886f7
shikhar <shikhargusain5@gmail.com>
2025-07-15 10:14:50 +0530

    15-07-2025

---
+++ input.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class input {
+public static void main(String[] args){
+    Scanner age=new Scanner(System.in);
+    Scanner name=new Scanner(System.in);
+    int x=age.nextInt();
+    String y=name.nextLine();
+    System.out.println(x);
+    System.out.print(y);
+}
+}
