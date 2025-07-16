5b73c9ad8b85bb42d3a887403e6a92cb4a2d2428
shikhar <shikhargusain5@gmail.com>
2025-07-16 10:29:07 +0530

    16-07-2025

---
+++ Switch.java
@@ -0,0 +1,14 @@
+public class Switch {
+    public static void main(String[] args){
+        int x=3;
+        switch(x){
+            case 1:System.out.print("1");
+                break;
+            case 2:System.out.print("2");
+                break;
+            case 3:System.out.print("3");
+                break;
+            default:System.out.print("invalid");
+        }
+    }
+}
