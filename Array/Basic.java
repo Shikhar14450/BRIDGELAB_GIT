bb3b1cf70f2b9c60d68fedb360903f8c523b9563
shikhar <shikhargusain5@gmail.com>
2025-07-17 12:03:03 +0530

    17-07-2025

---
+++ Array/Basic.java
@@ -0,0 +1,12 @@
+package Array;
+import java.util.*;
+public class Basic {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3};
+        for(int i=0;i<3;i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
+
+
