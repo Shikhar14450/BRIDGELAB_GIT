ddbd2c3e32d2efd02d56981b37d7abcce9a1aaa6
shikhar <shikhargusain5@gmail.com>
2025-07-22 11:39:17 +0530

    22-07-2025

--- Typecast.java
+++
@@ -1,8 +0,0 @@
-public class Typecast {
-    public static void main(String[] args){
-        double num1=50.49;
-        int num2=(int)num1;
-        System.out.println(num1);
-        System.out.println(num2);
-    }
-}
